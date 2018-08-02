aaafebb6a8e72b8ec4342fb22f9f4ccd7b9591a8
Izakk Carrillo <izakkcarrillo@skills-macbook-pro-15.local>
2018-08-02 14:47:50 -0400

    Finished the weather app

---
+++ Weather app/View Controllers/LocationSelectViewController.swift
@@ -0,0 +1,80 @@
+//
+//  LocationSelectViewController.swift
+//  Weather app
+//
+//  Created by Izakk Carrillo on 8/2/18.
+//  Copyright © 2018 Izakk Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class LocationSelectViewController: UIViewController, UISearchBarDelegate {
+    
+    let apiManager = APIManager()
+    
+    var geocodingData: GeocodingData?
+    var weatherData: WeatherData?
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchAddress = searchBar.text?.replacingOccurrences(of: " ", with: "+") else {
+            return
+        }
+        
+        apiManager.geocode(address: searchAddress, onSuccess: onRetrieving(geocodingData:), onError: errorRetrieved(error:))
+    }
+    
+    
+    func errorRetrieved(error: Error) {
+        geocodingData = nil
+        weatherData = nil
+    }
+    
+    func onRetrieving(geocodingData: GeocodingData) {
+        self.geocodingData = geocodingData
+        
+        apiManager.getWeather(latitude: geocodingData.latitude, longitude: geocodingData.longitude, onSuccess: onRetrieving(weatherData:), onError: errorRetrieved(error:))
+    }
+    
+    func onRetrieving(weatherData: WeatherData) {
+        self.weatherData = weatherData
+        self.performSegue(withIdentifier: "unwindToMainScreen", sender: self)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        guard let destinationVC = segue.destination as? WeatherViewController, let retrievedGeocodingData = geocodingData, let retrievedWeatherData = weatherData else {
+            return
+        }
+        
+        destinationVC.geocodingData = retrievedGeocodingData
+        destinationVC.weatherData = retrievedWeatherData
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
--- Weather app/View Controllers/WeatherViewController.swift
+++ Weather app/View Controllers/WeatherViewController.swift
@@ -16,19 +16,25 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var highTempLabel: UILabel!
     @IBOutlet weak var lowTempLabel: UILabel!
     
+    var weatherData: WeatherData! {
+        didSet {
+            iconLabel.text = weatherData.condition.icon
+            currentTempLabel.text = "\(weatherData.temperature)°"
+            highTempLabel.text = "\(weatherData.high)°"
+            lowTempLabel.text = "\(weatherData.low)°"
+        }
+    }
     
+    var geocodingData: GeocodingData! {
+        didSet {
+            locationLabel.text = geocodingData.formattedAddress
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setupUIWithoutData()
-        
-        let apiManager = APIManager()
-        apiManager.getWeather(latitude: 38.2527, longitude: -85.7585, onSuccess: { (WeatherData) in
-            
-        }) { (error) in
-            
-        }
     }
 
     func setupUIWithoutData() {
@@ -44,5 +50,6 @@ class WeatherViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func unwindToMainScreen(segue: UIStoryboardSegue) {
+    }
 }
